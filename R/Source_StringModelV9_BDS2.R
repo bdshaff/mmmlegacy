b4292c44645a1892df153b3c82f8d72b2792c62d
bdshaff <52110434+bdshaff@users.noreply.github.com>
2020-05-18 16:01:46 -0400

    change abc func
    change abc func

--- R/Source_StringModelV9_BDS2.R
+++ R/Source_StringModelV9_BDS2.R
@@ -1778,276 +1778,276 @@ ReachCurveCalc <- function(Values, input){
   return(AllReach)
 }
 
-ABCsReachGen <- function(Contrib, fit,  fit_displayt1, fit_displayt2, fit_st, fit_addressable,fmi_data, panel, spend, multiplier, year, From, To, revise = "NO", revise_table = Ctable_Revised) {
-
-  tolower(names(fmi_data)) -> names(fmi_data)
-
-  # Through Decomp
-  Contrib %>%
-    gather("Year", "Contributions",  5:ncol(.)) %>%
-    filter(Year == year) %>%
-    group_by(variable, Categories, Model, Year) %>%
-    summarise(Contributions = sum(Contributions)) %>%
-    mutate(TMP = 1) %>%
-    filter(grepl("_([E][0-9][R][0-9][P][0-9][0-9][D][0-9][0-9])", variable) | grepl("_([D][0-9][0-9][P][0-9])", variable)) %>%
-    arrange(variable) %>%
-    mutate(variable1 = variable) -> Contrib1
-
-  Contrib1 %>%
-    filter(grepl("_([D][0-9][0-9][P][0-9])", variable)) %>%
-    mutate(Type = "L") -> Contrib1a
-  Contrib1a$variable -> stockvars
-  tolower(stockvars) -> Contrib1a$variable1
-
-  Contrib1 %>%
-    filter(grepl("_([E][0-9][R][0-9][P][0-9][0-9][D][0-9][0-9])", variable)) %>%
-    mutate(Type = "R") -> Contrib1b
-
-  rbind(Contrib1a, Contrib1b) -> Contrib1
-
-  spend %>% filter(model_agg == panel | model_agg == toupper(panel)) -> spend
-  inner_join(Contrib1, spend, by = c("Categories" = "media_agg")) -> Contrib1
-  Contrib1 %>% filter(!is.na(Spend)) %>% filter(Contributions > 0) -> Contrib1
-
-
-  # Timeframe
-  timeframe <- as.data.frame(seq.Date(from = as.Date(From), to = as.Date(To), by = "week"))
-  names(timeframe) <- "week"
-  if (length(timeframe) > 104) stop("You need to select only 104 weeks")
-
-  # Parameters
-  param1 <- "_([E][0-9][R][0-9][P][0-9][0-9][D][0-9][0-9])" # formatting for adresponse
-  param2 <- "_([D][0-9][0-9][P][0-9])" # formatting for adstock
-  param3 <- "_([d][0-9][0-9][p][0-9])" # formatting for adstock
-
-
-  Contrib1$variable1 %>%
-    str_replace_all(param1, "") %>%
-    str_replace_all(param3, "") %>%
-    str_replace_all(., c("_log" = "", "PANEL_[A-Z][A-Z][A-Z]_M_" = "")) %>%
-    tolower(.) %>%
-    str_replace_all(paste(panel, "_", sep = ""), "") -> Allparams
-
-  Allparams -> Contrib1$LU
-
-  # Select Data
-  AllData <- as.data.frame(fmi_data[, colnames(fmi_data) %in% Allparams]) # grab columns to adresponse
-  AllData <- cbind(select(fmi_data, vehicle, week), AllData)
-  AllData <- AllData %>% filter(vehicle == panel)
-  AllData <- gather(AllData, "Category", "Value", 3:ncol(AllData))
-
-  DataJoin <- left_join(Contrib1[,c("variable", "Categories", "Model", "LU")], AllData, by = c("LU" = "Category"))
-  Data <- dcast(DataJoin, week  ~ Model + variable, fun.aggregate = sum, value.var = "Value")
-
-  # 104 Weeks
-  Data <- left_join(timeframe, Data)
-  names(Data)[1] <- "Week"
-  Data$Week <- NULL
-  neg_num <- as.data.frame(paste("Week", seq(from = -52, to = -1)))
-  names(neg_num) <- "Week"
-  pos_num <- as.data.frame(paste("Week", seq( from = 1, to = 52)))
-  names(pos_num) <- "Week"
-  Week <- rbind(neg_num, pos_num)
-  Data <- cbind(Week, Data)
-
-
-  #Parameters
-  Contrib1$E <- str_extract(Contrib1$variable1, "([E][0-9])")
-  Contrib1$E <- str_replace(Contrib1$E, "E", "")
-  Contrib1$R <- str_extract(Contrib1$variable1, "([R][0-9])")
-  Contrib1$R <- str_replace(Contrib1$R, "R", "")
-  Contrib1$P <- str_extract(Contrib1$variable1, "[P][0-9][0-9]")
-  Contrib1$P <- str_replace(Contrib1$P, "P", "")
-  Contrib1$P <- as.integer(Contrib1$P)
-  Contrib1$P <- Contrib1$P/7
-  Contrib1$D <- str_extract(Contrib1$variable1, "[D][0-9][0-9]")
-  Contrib1$D <- str_replace(Contrib1$D, "D", "")
-  Contrib1$p <- str_extract(Contrib1$variable1, "[p][0-9]")
-  Contrib1$p <- str_replace(Contrib1$p, "p", "")
-  Contrib1$d <- str_extract(Contrib1$variable1, "[d][0-9][0-9]")
-  Contrib1$d <- str_replace(Contrib1$d, "d", "")
-
-  # Input
-  paste(Contrib1$Model, Contrib1$variable, sep = "_") -> Contrib1$TableName
-  Contrib1 %>% arrange(TableName) -> Contrib1
-
-  # Revised
-  if (revise == "NO") {
-    Contrib1 -> Contrib1
-  } else {
-    revise_table -> Contrib1
-  }
-
-  Contrib1[,c("TableName", "Spend", "Contributions", "Type", "E", "R", "P", "D", "p", "d", "Categories")] -> Input
-  Input$p[c(!is.na(Input$p))] -> Input$P[c(is.na(Input$P))]
-  Input$d[c(!is.na(Input$d))] -> Input$D[c(is.na(Input$D))]
-  rbind(Input$Type, Input$Spend,  Input$Contributions, Input$E, Input$R, Input$P, Input$D, Input$Categories, Input$TableName) %>%
-    as.data.frame(.) -> Input
-
-
-  rbind(Input[c(1:3),], NA, Input[c(4:7),], NA, NA, NA, Input[c(8:9),]) -> Input
-
-  Col <- rbind("R/L/P", "100% Spend", "100% Contribution", "DMA", "EF", "RF", "Period", "Decay", "Alpha", "Beta", "Power", "Category", "Var Name")
-
-  Input <- cbind(Col, Input)
-
-  row.names(Input) <- NULL
-  Input <- as.data.frame(Input)
-  names(Input) <- as.character(unlist(Input[1,]))
-  Input = Input[-1,]
-  row.names(Input) <- NULL
-
-
-  fit <- as.data.frame(fit)
-  row.names(fit) <- NULL
-
-  # Write out
-  if (revise != "NO") {
-    write_csv(Contrib1, sprintf("%s RC Info Revised.csv", year))
-  } else {
-    write_csv(Contrib1, sprintf("%s RC Info.csv", year))
-  }
-
-  write_csv(Input, "Input.csv")
-  write_csv(Data, "Data.csv")
-  write_csv(fit, "fit.csv")
-  write_csv(fit_displayt1, "fit_displayt1.csv")
-  write_csv(fit_displayt2, "fit_displayt2.csv")
-  write_csv(fit_st, "fit_st.csv")
-  write_csv(fit_addressable, "fit_addressable.csv")
-
-  # Read in the template files
-  read_csv("Data.csv") -> Data
-  read.csv("Input.csv", check.names = FALSE, stringsAsFactors = FALSE) -> Input
-  read_csv("fit.csv") -> fit
-  read_csv("fit_displayt1.csv") ->fit_displayt1
-  read_csv("fit_displayt2.csv") ->fit_displayt2
-  read_csv("fit_st.csv") ->fit_st
-  read_csv("fit_addressable.csv") -> fit_addressable
-
-  # Generate Reach
-  names(fit)[1] <- "NTV"
-
-  ReachCurveCalc(Data, Input) -> AllReach
-  c(0:100) -> Percent
-  cbind(Percent, AllReach) -> AllReach1
-
-  gather(AllReach1, "Category", "Value", 2:ncol(AllReach1)) %>%
-    mutate(TableName = str_split_fixed(Category, " ", n = 2)[,1], Category = str_split_fixed(Category, " ", n = 2)[,2]) -> AllReach1
-
-  left_join(select(Contrib1, Categories, TableName), AllReach1, c("TableName" = "TableName")) %>%
-    spread(Category, Value) %>%
-    group_by(Categories, Percent) %>%
-    summarise(Contribution = sum(Contribution), Spend = mean(Spend), Reach = mean(Reach)) %>%
-    gather("Category", "Value", 3:5) %>%
-    arrange(desc(Category)) %>%
-    filter(Categories != "TV COMP") -> AllReach2
-
-  # Start ABCs
-  dcast(AllReach2, Percent ~ Categories + Category, fun.aggregate = sum, value.var = "Value") -> AllReach3
-  #AllReach3[c(1:101),] -> AllReach3
-  AllReach3$Percent <- NULL
-
-  # Reach
-  write_csv(AllReach3, "Reach Curve.csv")
-
-
-  AllReach3 -> totalmatrix
-  abcs <- list()
-  names(totalmatrix) <- rep(c("Contribution", "Reach", "Spend"), times = ncol(totalmatrix)/3)
-
-
-  dummy <- tribble(
-    ~term, ~estimate, ~std.error, ~statistic, ~p.value,
-    'a', 0, 0, 0, 0,
-    'b', 0, 0, 0, 0,
-    'c', 0, 0, 0, 0
-  )
-
-  # Loop Reach Curves to find ABCs
-  for (x in 1:(ncol(totalmatrix)/3)) {
-    # Find your ABCs
-    print(x)
-    data <- totalmatrix[,(1 + 3*(x - 1)):(3 + 3*(x - 1))]
-    model <- try(onls(Contribution ~ a/ (1 + b * ((Spend/1000)) ^ c),
-                      start  = list(a = 80, b = 1, c = -1),
-                      data = data,
-                      lower = c(a = 0.1, b = 0.1, c = -2)))
-
-    if(class(model) == "try-error") {
-      abcs[[x]] <- dummy
-      next;
-    }
-
-    m <- try({m <- broom::tidy(model)})
-    if(class(m) == "try-error") {
-      abcs[[x]] <- dummy
-      next;
-    }
-
-    abcs[[x]] <- m
-
-  }
-
-
-  abcs %>% bind_rows %>% select(estimate) -> abcs
-  data.frame(matrix(unlist(abcs), nrow = 3)) -> abcs
-  names(abcs) <- unique(AllReach2$Categories)
-
-  # Find real contributions
-  AllReach2 %>%
-    filter(Category == "Contribution") %>%
-    filter(Percent == 20) -> realcontributions
-
-  t(realcontributions[,4]) -> realcontributions
-
-  AllReach2 %>%
-    filter(Category == "Spend") %>%
-    filter(Percent == 20) -> realspend
-
-  t(realspend[,4]) -> realspend
-
-  # Adjust the A
-  mapply(Reach, abcs[1,], abcs[2,], abcs[3,], realspend[1,]) -> FirstContr
-  as.data.frame(t(FirstContr)) -> FirstContr
-  (realcontributions/FirstContr)*abcs[1,] -> AdjA
-  names(AdjA) <- names(abcs)
-  names(realcontributions) <- names(abcs)
-  names(realspend) <- names(abcs)
-  rbind(abcs, AdjA) -> ABCs
-  ABCs$Parameters <- c("A", "B", "C", "Adjusted A")
-  ABCs %>%
-    select(Parameters, 1:(ncol(ABCs) - 1)) -> ABCs
-  ABCs[,2:ncol(ABCs)] -> ABCs
-
-  mapply(Reach, ABCs[4,], ABCs[2,], ABCs[3,], realspend[1,]) -> FinalContr
-  as.data.frame(t(FinalContr)) -> FinalContr
-  names(FinalContr) <- names(ABCs)
-  rbind(ABCs, FinalContr) -> ABCs
-  as.data.frame(realspend) -> realspend
-  names(realspend) <- names(ABCs)
-  rbind(ABCs, realspend) -> ABCs
-  colABCs <- c("A", "B", "C", "Adjusted A", "Contribution", "Spend")
-  cbind(colABCs, ABCs) -> ABCs
-
-  if (multiplier > 0) {
-    # Revenue Calc
-    ABCs[5, c(2:ncol(ABCs))]*multiplier -> Revenue
-    rbind(ABCs[,c(2:ncol(ABCs))], Revenue) -> ABCs
-    # ROI Calc
-    ABCs[c(7),]/ABCs[c(6),] -> ROIs
-    rbind(ABCs, ROIs) -> ABCs
-    # mROI
-    mROIs <- ((mapply(Reach, ABCs[4,], ABCs[2,], ABCs[3,], ABCs[6,] + 10000)*multiplier) - (mapply(Reach, ABCs[4,], ABCs[2,], ABCs[3,], ABCs[6,])*multiplier))/10000
-    rbind(ABCs, mROIs) -> ABCs
-    # Labels
-    colABCs <- c("A", "B", "C", "Adjusted A", "Contribution", "Spend", "Revenue", "ROI", "mROI")
-    cbind(colABCs, ABCs) -> ABCs
-  } else {ABCs -> ABCs}
-
-  row.names(ABCs) <- NULL
-  return(ABCs)
-}
+# ABCsReachGen <- function(Contrib, fit,  fit_displayt1, fit_displayt2, fit_st, fit_addressable,fmi_data, panel, spend, multiplier, year, From, To, revise = "NO", revise_table = Ctable_Revised) {
+#
+#   tolower(names(fmi_data)) -> names(fmi_data)
+#
+#   # Through Decomp
+#   Contrib %>%
+#     gather("Year", "Contributions",  5:ncol(.)) %>%
+#     filter(Year == year) %>%
+#     group_by(variable, Categories, Model, Year) %>%
+#     summarise(Contributions = sum(Contributions)) %>%
+#     mutate(TMP = 1) %>%
+#     filter(grepl("_([E][0-9][R][0-9][P][0-9][0-9][D][0-9][0-9])", variable) | grepl("_([D][0-9][0-9][P][0-9])", variable)) %>%
+#     arrange(variable) %>%
+#     mutate(variable1 = variable) -> Contrib1
+#
+#   Contrib1 %>%
+#     filter(grepl("_([D][0-9][0-9][P][0-9])", variable)) %>%
+#     mutate(Type = "L") -> Contrib1a
+#   Contrib1a$variable -> stockvars
+#   tolower(stockvars) -> Contrib1a$variable1
+#
+#   Contrib1 %>%
+#     filter(grepl("_([E][0-9][R][0-9][P][0-9][0-9][D][0-9][0-9])", variable)) %>%
+#     mutate(Type = "R") -> Contrib1b
+#
+#   rbind(Contrib1a, Contrib1b) -> Contrib1
+#
+#   spend %>% filter(model_agg == panel | model_agg == toupper(panel)) -> spend
+#   inner_join(Contrib1, spend, by = c("Categories" = "media_agg")) -> Contrib1
+#   Contrib1 %>% filter(!is.na(Spend)) %>% filter(Contributions > 0) -> Contrib1
+#
+#
+#   # Timeframe
+#   timeframe <- as.data.frame(seq.Date(from = as.Date(From), to = as.Date(To), by = "week"))
+#   names(timeframe) <- "week"
+#   if (length(timeframe) > 104) stop("You need to select only 104 weeks")
+#
+#   # Parameters
+#   param1 <- "_([E][0-9][R][0-9][P][0-9][0-9][D][0-9][0-9])" # formatting for adresponse
+#   param2 <- "_([D][0-9][0-9][P][0-9])" # formatting for adstock
+#   param3 <- "_([d][0-9][0-9][p][0-9])" # formatting for adstock
+#
+#
+#   Contrib1$variable1 %>%
+#     str_replace_all(param1, "") %>%
+#     str_replace_all(param3, "") %>%
+#     str_replace_all(., c("_log" = "", "PANEL_[A-Z][A-Z][A-Z]_M_" = "")) %>%
+#     tolower(.) %>%
+#     str_replace_all(paste(panel, "_", sep = ""), "") -> Allparams
+#
+#   Allparams -> Contrib1$LU
+#
+#   # Select Data
+#   AllData <- as.data.frame(fmi_data[, colnames(fmi_data) %in% Allparams]) # grab columns to adresponse
+#   AllData <- cbind(select(fmi_data, vehicle, week), AllData)
+#   AllData <- AllData %>% filter(vehicle == panel)
+#   AllData <- gather(AllData, "Category", "Value", 3:ncol(AllData))
+#
+#   DataJoin <- left_join(Contrib1[,c("variable", "Categories", "Model", "LU")], AllData, by = c("LU" = "Category"))
+#   Data <- dcast(DataJoin, week  ~ Model + variable, fun.aggregate = sum, value.var = "Value")
+#
+#   # 104 Weeks
+#   Data <- left_join(timeframe, Data)
+#   names(Data)[1] <- "Week"
+#   Data$Week <- NULL
+#   neg_num <- as.data.frame(paste("Week", seq(from = -52, to = -1)))
+#   names(neg_num) <- "Week"
+#   pos_num <- as.data.frame(paste("Week", seq( from = 1, to = 52)))
+#   names(pos_num) <- "Week"
+#   Week <- rbind(neg_num, pos_num)
+#   Data <- cbind(Week, Data)
+#
+#
+#   #Parameters
+#   Contrib1$E <- str_extract(Contrib1$variable1, "([E][0-9])")
+#   Contrib1$E <- str_replace(Contrib1$E, "E", "")
+#   Contrib1$R <- str_extract(Contrib1$variable1, "([R][0-9])")
+#   Contrib1$R <- str_replace(Contrib1$R, "R", "")
+#   Contrib1$P <- str_extract(Contrib1$variable1, "[P][0-9][0-9]")
+#   Contrib1$P <- str_replace(Contrib1$P, "P", "")
+#   Contrib1$P <- as.integer(Contrib1$P)
+#   Contrib1$P <- Contrib1$P/7
+#   Contrib1$D <- str_extract(Contrib1$variable1, "[D][0-9][0-9]")
+#   Contrib1$D <- str_replace(Contrib1$D, "D", "")
+#   Contrib1$p <- str_extract(Contrib1$variable1, "[p][0-9]")
+#   Contrib1$p <- str_replace(Contrib1$p, "p", "")
+#   Contrib1$d <- str_extract(Contrib1$variable1, "[d][0-9][0-9]")
+#   Contrib1$d <- str_replace(Contrib1$d, "d", "")
+#
+#   # Input
+#   paste(Contrib1$Model, Contrib1$variable, sep = "_") -> Contrib1$TableName
+#   Contrib1 %>% arrange(TableName) -> Contrib1
+#
+#   # Revised
+#   if (revise == "NO") {
+#     Contrib1 -> Contrib1
+#   } else {
+#     revise_table -> Contrib1
+#   }
+#
+#   Contrib1[,c("TableName", "Spend", "Contributions", "Type", "E", "R", "P", "D", "p", "d", "Categories")] -> Input
+#   Input$p[c(!is.na(Input$p))] -> Input$P[c(is.na(Input$P))]
+#   Input$d[c(!is.na(Input$d))] -> Input$D[c(is.na(Input$D))]
+#   rbind(Input$Type, Input$Spend,  Input$Contributions, Input$E, Input$R, Input$P, Input$D, Input$Categories, Input$TableName) %>%
+#     as.data.frame(.) -> Input
+#
+#
+#   rbind(Input[c(1:3),], NA, Input[c(4:7),], NA, NA, NA, Input[c(8:9),]) -> Input
+#
+#   Col <- rbind("R/L/P", "100% Spend", "100% Contribution", "DMA", "EF", "RF", "Period", "Decay", "Alpha", "Beta", "Power", "Category", "Var Name")
+#
+#   Input <- cbind(Col, Input)
+#
+#   row.names(Input) <- NULL
+#   Input <- as.data.frame(Input)
+#   names(Input) <- as.character(unlist(Input[1,]))
+#   Input = Input[-1,]
+#   row.names(Input) <- NULL
+#
+#
+#   fit <- as.data.frame(fit)
+#   row.names(fit) <- NULL
+#
+#   # Write out
+#   if (revise != "NO") {
+#     write_csv(Contrib1, sprintf("%s RC Info Revised.csv", year))
+#   } else {
+#     write_csv(Contrib1, sprintf("%s RC Info.csv", year))
+#   }
+#
+#   write_csv(Input, "Input.csv")
+#   write_csv(Data, "Data.csv")
+#   write_csv(fit, "fit.csv")
+#   write_csv(fit_displayt1, "fit_displayt1.csv")
+#   write_csv(fit_displayt2, "fit_displayt2.csv")
+#   write_csv(fit_st, "fit_st.csv")
+#   write_csv(fit_addressable, "fit_addressable.csv")
+#
+#   # Read in the template files
+#   read_csv("Data.csv") -> Data
+#   read.csv("Input.csv", check.names = FALSE, stringsAsFactors = FALSE) -> Input
+#   read_csv("fit.csv") -> fit
+#   read_csv("fit_displayt1.csv") ->fit_displayt1
+#   read_csv("fit_displayt2.csv") ->fit_displayt2
+#   read_csv("fit_st.csv") ->fit_st
+#   read_csv("fit_addressable.csv") -> fit_addressable
+#
+#   # Generate Reach
+#   names(fit)[1] <- "NTV"
+#
+#   ReachCurveCalc(Data, Input) -> AllReach
+#   c(0:100) -> Percent
+#   cbind(Percent, AllReach) -> AllReach1
+#
+#   gather(AllReach1, "Category", "Value", 2:ncol(AllReach1)) %>%
+#     mutate(TableName = str_split_fixed(Category, " ", n = 2)[,1], Category = str_split_fixed(Category, " ", n = 2)[,2]) -> AllReach1
+#
+#   left_join(select(Contrib1, Categories, TableName), AllReach1, c("TableName" = "TableName")) %>%
+#     spread(Category, Value) %>%
+#     group_by(Categories, Percent) %>%
+#     summarise(Contribution = sum(Contribution), Spend = mean(Spend), Reach = mean(Reach)) %>%
+#     gather("Category", "Value", 3:5) %>%
+#     arrange(desc(Category)) %>%
+#     filter(Categories != "TV COMP") -> AllReach2
+#
+#   # Start ABCs
+#   dcast(AllReach2, Percent ~ Categories + Category, fun.aggregate = sum, value.var = "Value") -> AllReach3
+#   #AllReach3[c(1:101),] -> AllReach3
+#   AllReach3$Percent <- NULL
+#
+#   # Reach
+#   write_csv(AllReach3, "Reach Curve.csv")
+#
+#
+#   AllReach3 -> totalmatrix
+#   abcs <- list()
+#   names(totalmatrix) <- rep(c("Contribution", "Reach", "Spend"), times = ncol(totalmatrix)/3)
+#
+#
+#   dummy <- tribble(
+#     ~term, ~estimate, ~std.error, ~statistic, ~p.value,
+#     'a', 0, 0, 0, 0,
+#     'b', 0, 0, 0, 0,
+#     'c', 0, 0, 0, 0
+#   )
+#
+#   # Loop Reach Curves to find ABCs
+#   for (x in 1:(ncol(totalmatrix)/3)) {
+#     # Find your ABCs
+#     print(x)
+#     data <- totalmatrix[,(1 + 3*(x - 1)):(3 + 3*(x - 1))]
+#     model <- try(onls(Contribution ~ a/ (1 + b * ((Spend/1000)) ^ c),
+#                       start  = list(a = 80, b = 1, c = -1),
+#                       data = data,
+#                       lower = c(a = 0.1, b = 0.1, c = -2)))
+#
+#     if(class(model) == "try-error") {
+#       abcs[[x]] <- dummy
+#       next;
+#     }
+#
+#     m <- try({m <- broom::tidy(model)})
+#     if(class(m) == "try-error") {
+#       abcs[[x]] <- dummy
+#       next;
+#     }
+#
+#     abcs[[x]] <- m
+#
+#   }
+#
+#
+#   abcs %>% bind_rows %>% select(estimate) -> abcs
+#   data.frame(matrix(unlist(abcs), nrow = 3)) -> abcs
+#   names(abcs) <- unique(AllReach2$Categories)
+#
+#   # Find real contributions
+#   AllReach2 %>%
+#     filter(Category == "Contribution") %>%
+#     filter(Percent == 20) -> realcontributions
+#
+#   t(realcontributions[,4]) -> realcontributions
+#
+#   AllReach2 %>%
+#     filter(Category == "Spend") %>%
+#     filter(Percent == 20) -> realspend
+#
+#   t(realspend[,4]) -> realspend
+#
+#   # Adjust the A
+#   mapply(Reach, abcs[1,], abcs[2,], abcs[3,], realspend[1,]) -> FirstContr
+#   as.data.frame(t(FirstContr)) -> FirstContr
+#   (realcontributions/FirstContr)*abcs[1,] -> AdjA
+#   names(AdjA) <- names(abcs)
+#   names(realcontributions) <- names(abcs)
+#   names(realspend) <- names(abcs)
+#   rbind(abcs, AdjA) -> ABCs
+#   ABCs$Parameters <- c("A", "B", "C", "Adjusted A")
+#   ABCs %>%
+#     select(Parameters, 1:(ncol(ABCs) - 1)) -> ABCs
+#   ABCs[,2:ncol(ABCs)] -> ABCs
+#
+#   mapply(Reach, ABCs[4,], ABCs[2,], ABCs[3,], realspend[1,]) -> FinalContr
+#   as.data.frame(t(FinalContr)) -> FinalContr
+#   names(FinalContr) <- names(ABCs)
+#   rbind(ABCs, FinalContr) -> ABCs
+#   as.data.frame(realspend) -> realspend
+#   names(realspend) <- names(ABCs)
+#   rbind(ABCs, realspend) -> ABCs
+#   colABCs <- c("A", "B", "C", "Adjusted A", "Contribution", "Spend")
+#   cbind(colABCs, ABCs) -> ABCs
+#
+#   if (multiplier > 0) {
+#     # Revenue Calc
+#     ABCs[5, c(2:ncol(ABCs))]*multiplier -> Revenue
+#     rbind(ABCs[,c(2:ncol(ABCs))], Revenue) -> ABCs
+#     # ROI Calc
+#     ABCs[c(7),]/ABCs[c(6),] -> ROIs
+#     rbind(ABCs, ROIs) -> ABCs
+#     # mROI
+#     mROIs <- ((mapply(Reach, ABCs[4,], ABCs[2,], ABCs[3,], ABCs[6,] + 10000)*multiplier) - (mapply(Reach, ABCs[4,], ABCs[2,], ABCs[3,], ABCs[6,])*multiplier))/10000
+#     rbind(ABCs, mROIs) -> ABCs
+#     # Labels
+#     colABCs <- c("A", "B", "C", "Adjusted A", "Contribution", "Spend", "Revenue", "ROI", "mROI")
+#     cbind(colABCs, ABCs) -> ABCs
+#   } else {ABCs -> ABCs}
+#
+#   row.names(ABCs) <- NULL
+#   return(ABCs)
+# }
 
 
 
